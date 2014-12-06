89981a70ad41920641f766b1e425be203a493330
Kennysandbum <kenneth_sanders@student.uml.edu>
2014-12-06 09:21:33 -0500

    stuff

--- Fingerprint/src/com/example/fingerprint/Binarizer.java
+++ Fingerprint/src/com/example/fingerprint/Binarizer.java
@@ -19,6 +19,7 @@ import Catalano.Imaging.Filters.FourierTransform;
 import Catalano.Imaging.Filters.FrequencyFilter;
 import Catalano.Imaging.Filters.HistogramEqualization;
 import Catalano.Imaging.Filters.Sharpen;
+import Catalano.Imaging.Filters.Threshold;
 
 
 /**
@@ -62,7 +63,7 @@ public class Binarizer extends Activity{
     
     
     public void testThings() {
-
+/* THIS WORKS
         SusanCornersDetector susanCornersDetector = new SusanCornersDetector();
         ArrayList<IntPoint> list = susanCornersDetector.ProcessImage(img);
         for (IntPoint p : list) {
@@ -100,8 +101,9 @@ public class Binarizer extends Activity{
         ff.ApplyInPlace(ft);
         ft.Backward();
         Log.d("testThings", "went backward in ft");
-        img = ft.toFastBitmap();
+        img = ft.toFastBitmap();*/
 
+        divideAndConquer(0, 0, 4);
 
 
         //this hist eq doesnt work here
@@ -110,20 +112,26 @@ public class Binarizer extends Activity{
 
         //CannyEdgeDetector cannyEdgeDetector = new CannyEdgeDetector(10,20);
         //cannyEdgeDetector.applyInPlace(img);
-
+/*
         FastBitmap temp;
         int i, j, k, l, cellsize = 3;
-        for (i = 0; i < img.getWidth() / cellsize - 1; i++) {
+        for (i = 0; i < (img.getWidth() / cellsize) -1; i++) {
             Log.d("testThings", "i = " + i);
-            for (j = 0; j < img.getHeight() / cellsize - 1; i++) {
+            for (j = 0; j < (img.getHeight() / cellsize) -1; j++) {
                 temp = new FastBitmap(cellsize, cellsize);
                 Log.d("testThings", "j = " + j);
                 for (k = 0; k < cellsize; k++) {
                     Log.d("testThings", "k = " + k);
                     for (l = 0; l < cellsize; l++) {
                         temp.setRGB(k, l, img.getRGB(i * cellsize + k, j * cellsize + l));
-                        Sharpen sharpen = new Sharpen();
-                        sharpen.applyInPlace(temp);
+                        Log.d("testThings", "i = " + i + ", j = " + j + ", k = " + k + ", l = " + l + ",  at pixel " + i * cellsize + "x" +j*cellsize);
+                    }
+                }
+                Sharpen sharpen = new Sharpen();
+                sharpen.applyInPlace(temp);
+                for (k = 0; k < cellsize; k++) {
+                    Log.d("testThings", "k = " + k);
+                    for (l = 0; l < cellsize; l++) {
                         Log.d("testThings", "i = " + i + ", j = " + j + ", k = " + k + ", l = " + l + ",  at pixel " + i * cellsize + "x" +j*cellsize);
                         img.setRGB(i * cellsize + k, j * cellsize + l, temp.getRGB(k, l));
                         Log.d("testThings", "cycled");
@@ -132,46 +140,120 @@ public class Binarizer extends Activity{
                 temp.recycle();
                 Log.d("testThings", "recycled");
             }
-        }
+        }*/
 
             //SobelEdgeDetector sobelEdgeDetector = new SobelEdgeDetector();
             //sobelEdgeDetector.applyInPlace(img);
 
 
-            BradleyLocalThreshold bradleyLocalThreshold = new BradleyLocalThreshold(160);
-            bradleyLocalThreshold.applyInPlace(img);
+            //BradleyLocalThreshold bradleyLocalThreshold = new BradleyLocalThreshold(160);
+            //bradleyLocalThreshold.applyInPlace(img);
 
             //Threshold threshold = new Threshold(15);
             //threshold.applyInPlace(img);
 
 
+
+
             //Threshold with divide and conquer iterative
-        /*int i, j, xpix, ypix, xmax = img.toBitmap().getWidth(), ymax = img.toBitmap().getHeight(), xcells = 2, ycells = 2, xcellsize = xmax / xcells, ycellsize = ymax / ycells;
-        Bitmap[][] arr = new Bitmap[xmax][ymax];
-        Bitmap newimg = Bitmap.createBitmap(img.toBitmap()), tempbm = null;
+        /*int i, j, xpix, ypix, xmax = img.getWidth(), ymax = img.getHeight(), xcells = 2, ycells = 2, xcellsize = xmax / xcells, ycellsize = ymax / ycells;
+        FastBitmap[][] arr = new FastBitmap[xmax][ymax];
         for (i = 0; i < xcells; i++) {
             for (j = 0; j < ycells; j++) {
-                arr[i][j] = Bitmap.createBitmap(img.toBitmap(), i * xcellsize, j * ycellsize, xmax - i * xcellsize, ymax - j * ycellsize);
-                FastBitmap fbm = new FastBitmap(arr[i][j]);
-                fbm.toGrayscale();
+                arr[i][j] = new FastBitmap(xcellsize, ycellsize);
+                for (xpix = 0; xpix < xcellsize; xpix++) {
+                    Log.d("testThings", "xpix = " + xpix);
+                    for (ypix = 0; ypix < ycellsize; ypix++) {
+                        arr[i][j].setRGB(xpix, ypix, img.getRGB(i * xcellsize + xpix, j * ycellsize + ypix));
+                        Log.d("testThings", "i = " + i + ", j = " + j + ", xpix = " + xpix + ", ypix = " + ypix + ",  at pixel " + i * xcellsize + "x" + j * ycellsize);
+                    }
+                }
+
+                arr[i][j].toGrayscale();
+                Sharpen sharpen = new Sharpen();
+                sharpen.applyInPlace(img);
                 Log.d("binarizer", "got fbm in gray");
                 Threshold threshold = new Threshold(30);
-                threshold.applyInPlace(fbm);
+                threshold.applyInPlace(arr[i][j]);
                 Log.d("binarizer", "applied threshold");
-                tempbm = fbm.toBitmap();
                 Log.d("binarizer", "entering for loop ");
-                for (xpix = 0; xpix < tempbm.getWidth(); xpix++) {
-                    for (ypix = 0; ypix < tempbm.getHeight(); ypix++) {
-                        newimg.setPixel(i * xcellsize + xpix, j * ycellsize + ypix, tempbm.getPixel(xpix, ypix));
+                for (xpix = 0; xpix < arr[i][j].getWidth(); xpix++) {
+                    for (ypix = 0; ypix < arr[i][j].getHeight(); ypix++) {
+                        img.setRGB(i * xcellsize + xpix, j * ycellsize + ypix, arr[i][j].getRGB(xpix, ypix));
                         Log.d("binarizer", "i =" + i + ", j =" + j +", xpix =" + xpix + ", ypix =" + ypix);
                     }
                 }
+                arr[i][j].recycle();
                 Log.d("binarizer", "copied bm with i = " + i + " and j = " + j);
 
             }
+        }*/
+    }
+
+    //Recurssion dividing
+    public FastBitmap copyPixels(int tox, int toy, int fromx, int fromy, int startx, int starty, FastBitmap to, FastBitmap from){
+        int w, h;
+        if(from.getWidth() < to.getWidth()) {
+            w = from.getWidth();
         }
-        img.recycle();
-        img = new FastBitmap(newimg);*/
+        else{
+            w = to.getWidth();
+        }
+        if(from.getHeight() < to.getHeight()) {
+            h = from.getWidth();
+        }
+        else {
+            h = to.getWidth();
+        }
+        h--;
+        w--;
+
+        Log.d("copyPixels", "w = " + w + ", h = " + h + ", startx = " +startx + " , starty = " + starty);
+        Log.d("copyPixels", "tox = " + tox + ", toy = " + toy+ ", fromx = " + fromx + ", fromy = " + fromy);
+
+        if(((fromx < w && tox - startx < w) || (fromx - startx < w && tox < w))  && ((fromy - starty < h || toy < h) && (fromy < h || toy - starty < h))){//problem is here
+            to.setRGB(tox, toy, from.getRGB(fromx, fromy));
+            Log.d("copyPixels", "copied pixel");
+            return copyPixels(tox + 1, toy, fromx + 1, fromy, startx, starty, to, from);
+        }
+        else if((((fromy - starty < h && toy < h)||(fromy < h && toy - starty < h)|| (fromy < h && toy < h)) && ((fromx == w && tox - startx == w)|| (fromx - startx == w && tox == w)))){
+            Log.d("copyPixels", "past if in else");
+            return copyPixels(tox - w, toy + 1, fromx - w, fromy + 1, startx, starty, to, from);
+        }
+        else {
+            Log.d("copyPixels", "past else if");
+            return to;
+        }
+    }
+
+    public void divideAndConquer(int x, int y, int cellsize){
+        if (x == img.getWidth() && y == img.getHeight())
+            return;
+//It reaches 768 for x in this function, causin out of bounds error   
+        else if (x + cellsize < img.getWidth() && y < img.getHeight()) {
+            FastBitmap cell = new FastBitmap(cellsize, cellsize);
+            cell = copyPixels(0, 0, x, y, x, y, cell, img);
+            /*
+            filters
+            Sharpen sharpen = new Sharpen();
+            sharpen.applyInPlace(cell);
+            */
+            cell.toGrayscale();
+            BradleyLocalThreshold bradleyLocalThreshold = new BradleyLocalThreshold(160);
+            bradleyLocalThreshold.applyInPlace(cell);
+            Log.d("divideAndConquer", "filtered at x = " + x + ", y = " + y);
+            img = copyPixels(x, y, 0, 0, x, y, img, cell);
+            cell.recycle();
+            Log.d("divideAndConquer", "opening new div/conq");
+            divideAndConquer(x + cellsize, y, cellsize);
+            return;
+        } else if (y < img.getHeight()) {
+            divideAndConquer(0, y + cellsize, cellsize);
+            return;
+        } else {
+            Log.d("divideAndConquer", "uh oh");
+            return;
+        }
     }
 
     public Bitmap getBitmap(){
